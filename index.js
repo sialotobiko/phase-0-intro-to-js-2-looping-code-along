38b60135c1e8b6e6beaeba8100f8d09bcc142571
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-08-20 17:32:33 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(friends, event){
+    
+    let friends1 = [];
+
+    for(let i = 0; i < friends.length; i++){
+        friends1.push(`Thank you, ${friends[i]}, for the wonderful ${event} gift!`)
+    }
+    return friends1;
+}
+
+function countDown(i){
+    
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
